2244a0271a5c6a213993fd3ad84b61e6002ae5b3
Geovany Cardoza <geoyd17@gmail.com>
2020-11-05 15:52:57 -0500

    setting up folder structure, routes folder, server.js, components folder controller folder, models folder

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <h1>hello</h1>
+      </Router>
+    </>
+  );
+}
+
+export default App;
