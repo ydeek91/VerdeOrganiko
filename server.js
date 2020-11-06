bece8b391d42cf789a583b4ed012d93a85886a29
Geovany Cardoza <geoyd17@gmail.com>
2020-11-05 23:47:39 -0500

    modifing the models folder files

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Define API routes here
+app.use(routes);
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+//Connet to mongoose
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/verdeorganico",
+  {
+    useNewUrlParser: true,
+  }
+);
+
+app.listen(PORT, function () {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
