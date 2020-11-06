45ac2b969276bd3df173168b80f91e203d321b74
Nodirbek94 <nodirbekmaksudov@yahoo.com>
2020-11-06 14:15:53 -0500

    added multiple pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,27 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import LoginForm from "./components/LoginForm/LoginForm";
+import Product from "./Pages/Product";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
 
 function App() {
   return (
     <>
       <Router>
         <Navbar />
-        <Hero />
-        <h2 class="ui center aligned icon header">
+        {/* <Hero /> */}
+        {/* <h2 class="ui center aligned icon header">
           <i class="circular users icon"></i>
           About us
-        </h2>
-
-        <LoginForm />
+        </h2> */}
+        <Route path='/buy' exact component={Product} />
+        <Route path='/home' exact component={Home} />
+        <Route path='/login' exact component={Login} />
+        {/* <LoginForm /> */}
       </Router>
     </>
   );
