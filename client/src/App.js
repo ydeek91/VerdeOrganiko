56bc3ee2882aa2e05f4554b681d0c978c9d1a63b
Geovany Cardoza <geoyd17@gmail.com>
2020-11-05 23:44:41 -0500

    made a Navbar component , a hero image component , models for plants and for orders  routes folder, semantic cnd and some dependecies

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import "./App.css";
+import "semantic-ui-css/semantic.min.css";
+import Hero from "./components/Hero/Hero";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
       <Router>
-        <h1>hello</h1>
+        <Navbar />
+        <Hero />
+        <h2 class="ui center aligned icon header">
+          <i class="circular users icon"></i>
+          About us
+        </h2>
       </Router>
     </>
   );
---
+++ client/src/components/Hero/Hero.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "semantic-ui-css/semantic.min.css";
+
+const Hero = () => (
+  <>
+    <div class="ui inverted vertical masthead center aligned segment">
+      <div class="ui text container">
+        <div class="ui huge primary button">
+          Welcome <i class="right arrow icon"></i>
+        </div>
+      </div>
+    </div>
+  </>
+);
+
+export default Hero;
