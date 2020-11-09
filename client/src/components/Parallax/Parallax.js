b43ee1204e0d55d9d9db981647716743b425ee51
Geovany Cardoza <geoyd17@gmail.com>
2020-11-09 11:58:10 -0500

    making css style file

--- client/src/components/Parallax/Parallax.js
+++ client/src/components/Parallax/Parallax.js
@@ -4,7 +4,7 @@ import About from "../About/About";
 import About2 from "../About/About2";
 import About3 from "../About/About3";
 import About4 from "../About/About4";
-
+import "./Parallax.css";
 const Parallax = () => (
   <>
     <section>
