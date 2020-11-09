be576b35b5ad63614bf0e394e3c84281ef3ae9c1
Yazan Deek <ydeek91@yahoo.com>
2020-11-09 09:03:19 -0500

    wrote actions.js code

--- client/src/components/SignUp/actions.js
+++ client/src/components/SignUp/actions.js
@@ -19,3 +19,73 @@ import { allFieldsValidation } from '../../utils/validation';
 export const signupChange = (name, value) => {
     let formData = {};
     formData[name] = value;
+
+
+    return {
+        type: SIGNUP_CHANGE,
+        payload: formData
+    };
+};
+
+export const subscribeChange = () => {
+    return {
+        type: SUBSCRIBE_CHANGE
+    };
+};
+
+export const signUp = () => {
+    return async (dispatch, getState) => {
+        try {
+            const rules = {
+                email: 'required|email',
+                password: 'required|min:6',
+                firstName: 'required',
+                lastName: 'required'
+            };
+
+            const newUser = getState().signup.signupFormData;
+            const isSubscribed = getState().signup.isSubscribed;
+
+            const { isValid, errors } = allFieldsValidation(newUser, rules, {
+                'required.email': 'Email is required.',
+                'required.password': 'Password is required.',
+                'required.firstName': 'First Name is required.',
+                'required.lastName': 'Last Name is required.'
+            });
+
+            if (!isValid) {
+                return dispatch({ type: SET_SIGNUP_FORM_ERRORS, payload: errors });
+            }
+
+            dispatch({ type: SET_SIGNUP_SUBMITTING, payload: true });
+            dispatch({ type: SET_SIGNUP_LOADING, payload: true });
+
+            const user = {
+                isSubscribed,
+                ...newUser
+            };
+
+            const response = await axios.post('/api/auth/register', user);
+
+            const successfulOptions = {
+                title: `You have signed up successfully! You will be receiving an email as well. Thank you!`,
+                position: 'tr',
+                autoDismiss: 1
+            };
+
+            localStorage.setItem('token', response.data.token);
+
+            setToken(response.data.token);
+
+            dispatch(setAuth());
+            dispatch(success(successfulOptions));
+            dispatch({ type: SIGNUP_RESET });
+        } catch (error) {
+            const title = `Please try to signup again!`;
+            handleError(error, dispatch, title);
+        } finally {
+            dispatch({ type: SET_SIGNUP_SUBMITTING, payload: false });
+            dispatch({ type: SET_SIGNUP_LOADING, payload: false });
+        }
+    };
+};
