60a2b22682c264012286c0aa0ddea0b37316b61c
Yazan Deek <ydeek91@yahoo.com>
2020-11-09 08:57:32 -0500

    wrote imports for index.js

---
+++ client/src/components/SignUp/index.js
@@ -0,0 +1,32 @@
+/*
+ *
+ * Signup
+ *
+ */
+
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { Row, Col } from 'reactstrap';
+import { Redirect, Link } from 'react-router-dom';
+
+import actions from '../../actions';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import LoadingIndicator from '../../components/LoadingIndicator';
+import SignupProvider from '../../components/SignupProvider';
+import Checkbox from '../../components/Checkbox';
+
+class Signup extends React.PureComponent {
+    render() {
+        const {
+            authenticated,
+            signupFormData,
+            formErrors,
+            isLoading,
+            isSubmitting,
+            isSubscribed,
+            signupChange,
+            signUp,
+            subscribeChange
+        } = this.props;
